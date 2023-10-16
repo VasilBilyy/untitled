66517df7023c6d065608d9f25b490add7677dae5
VasilBilyy <bilyjvasil@gmail.com>
2023-10-16 23:38:57 +0300

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -23,8 +23,11 @@ public class Main {
         insert_in_begin(linkedList, insertCount, "LinkedList");
         insert_in_end(arrayList, insertCount, "ArrayList");
         insert_in_end(linkedList, insertCount, "LinkedList");
+        insert_in_middle(arrayList, insertCount, "ArrayList");
+        insert_in_middle(linkedList, insertCount, "LinkedList");
     }
 
+    //Заповнення масиву
     private static void fill(List<Integer> list, int count, String listType) {
         long time = System.currentTimeMillis();
         Random random = new Random();
@@ -37,6 +40,7 @@ public class Main {
         System.out.printf("Fill %s: %s\n", listType, System.currentTimeMillis() - time);
     }
 
+    //Random Access (доступ за індексом)
     private static void random_access(List<Integer> list, int count, String listType){
         long time = System.currentTimeMillis();
         Random random = new Random();
@@ -45,6 +49,7 @@ public class Main {
         System.out.printf("Random access in %s: %s\n", listType, System.currentTimeMillis() - time);
     }
 
+    //Sequential Access (доступ по ітератору)
     private static void sequential_access(List<Integer> list, String listType){
         long time = System.currentTimeMillis();
         Iterator<Integer> itr = list.iterator();
@@ -55,6 +60,7 @@ public class Main {
         System.out.printf("Sequental access in %s: %s\n", listType, System.currentTimeMillis() - time);
     }
 
+    //Вставка на початок списку
     private static void insert_in_begin(List<Integer> list, int insertCount, String listType){
         long time = System.currentTimeMillis();
         Random random = new Random();
@@ -68,6 +74,7 @@ public class Main {
         System.out.printf("Insert in the beginning of %s: %s\n", listType, System.currentTimeMillis() - time);
     }
 
+    //Вставка у кінець списку
     private static void insert_in_end(List<Integer> list, int insertCount, String listType){
         long time = System.currentTimeMillis();
         Random random = new Random();
@@ -75,8 +82,22 @@ public class Main {
         for (int i = 0; i < insertCount; i++){
             list.add(random.nextInt(insertCount));
         }
-        System.out.println(list.size());
+
 
         System.out.printf("Insert in the end of %s: %s\n", listType, System.currentTimeMillis() - time);
+    }
+
+    //Вставка до середини списку
+    private static void insert_in_middle(List<Integer> list, int insertCount, String listType){
+        long time = System.currentTimeMillis();
+        int start_position = list.size() / 2 - insertCount / 2;
+        int final_position = start_position + insertCount;
+        Random random = new Random();
+
+        for (int i = start_position; i< final_position; i++){
+            list.add(i, random.nextInt(insertCount));
+        }
+
+        System.out.printf("Insert in the middle of %s: %s\n", listType, System.currentTimeMillis() - time);
     }
 }
