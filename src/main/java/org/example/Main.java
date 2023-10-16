3c17828d64ade72c7a0af688208dc62541e1f80c
VasilBilyy <bilyjvasil@gmail.com>
2023-10-16 21:29:41 +0300

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,17 +2,27 @@ package org.example;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
         List<Integer> arrayList = new ArrayList<>();
         List<Integer> linkedList = new LinkedList<>();
+
+        int count = 100000;
+        int insertCount = 1000;
+
+        fill(arrayList, count, "ArrayList");
     }
 
     private static void fill(List<Integer> list, int count, String listType) {
         long time = System.currentTimeMillis();
+        Random random = new Random();
+        for (int i = 0; i < count; i++){
+            list.add(random.nextInt(count + 1));
+            System.out.print(list.get(i)+"\n");
+        }
 
-
-        System.out.printf("Fill %s: %s/n", listType, System.currentTimeMillis() - time);
+        System.out.printf("Fill %s: %s\n", listType, System.currentTimeMillis() - time);
     }
 }
